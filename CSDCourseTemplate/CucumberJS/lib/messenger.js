9d9c1b06e4d44793000060da013b98aa2986498b
Montserrat Camarillo <Montserrat.Camarillo@compac.com.mx>
2020-02-11 17:58:58 -0600

    ingles

--- CSDCourseTemplate/CucumberJS/lib/messenger.js
+++ CSDCourseTemplate/CucumberJS/lib/messenger.js
@@ -1,23 +1,38 @@
 class Messenger {
 
     constructor( ) {
-        
+        this.languagedic = {
+            Español: [
+                "Buenos dias",
+                "Buenas tardes",
+                "Buenas noches",
+                "error - formato no valido"
+            ],
+            English: [
+                "Good Morning",
+                "Good Afternoon",
+                "Good Night",
+                "err - format not valid"
+            ]
+        }
     }
 
-    getMessage(time) {
+    getMessage(time,language) {
         if (time=== undefined){
         return "Hello World!";
         }
+    
+        var messages=this.languagedic[language]
         
-        if (new Date(time)=="Invalid Date") { return("error - formato no valido")}
-        if (time==null) { return("error - formato no valido")}
+        if (new Date(time)=="Invalid Date") { return(messages[3])}
+        if (time==null) { return(messages[3])}
         let dateTime =  new Date(time);
         if((dateTime.getHours() >= 0) && (dateTime.getHours() < 12)){
-            return "Buenos dias";
+            return messages[0];
         }else  if((dateTime.getHours() >= 12) && (dateTime.getHours() < 18)){
-            return "Buenas tardes";
+            return messages[1];
         }else  if((dateTime.getHours() >= 18) && (dateTime.getHours() <= 23)){
-            return "Buenas noches";
+            return messages[2];
         }
     } 
 }
